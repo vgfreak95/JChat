15532972020cb4b890af5d4934ce25dd4a0546bc
David <djwhalen@uncg.edu>
2022-03-31 17:12:59 -0400

    tweaks to server information handling added server and serverworker classes

--- src/main/java/com/example/jchat/Server.java
+++ src/main/java/com/example/jchat/Server.java
@@ -4,7 +4,7 @@ import java.io.*;
 import java.net.*;
 import java.nio.charset.StandardCharsets;
 
-public class Server extends SocketAddress implements Runnable {
+public class Server extends SocketAddress {
 
     public static void main(String[] args){
         int portNumber = 8000;
@@ -12,30 +12,17 @@ public class Server extends SocketAddress implements Runnable {
             ServerSocket serverSocket = new ServerSocket(portNumber);
             while(true){
                 Socket clientSocket = serverSocket.accept();
-                Server s = new Server();
-                if(clientSocket.isConnected()){
-                    s.run();
-                }
-//                ServerWork work = new ServerWork(clientSocket);
-//                work.start;
 
                 System.out.println("Connection established with: " +clientSocket);
                 OutputStream outputStream = clientSocket.getOutputStream();
-                outputStream.write("FUCK this did work\n".getBytes());
-                clientSocket.close();
+                outputStream.write("FUCK this did work\r\n".getBytes());
+                ServerWork work = new ServerWork(clientSocket);
+                work.start();
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
 
 
-    }
-
-    @Override
-    public void run() {
-
-    }
-    private void serverLoop(){
-//        InputStream inputStream =
     }
 }
--- src/main/java/com/example/jchat/ServerWork.java
+++ src/main/java/com/example/jchat/ServerWork.java
@@ -1,8 +1,44 @@
-//package com.example.jchat;
-//
-//import java.net.Socket;
-//
-//public class ServerWork {
-//    public ServerWork(Socket clientSocket) {
-//    }
-//}
+package com.example.jchat;
+
+import javafx.scene.control.TextField;
+
+import java.io.*;
+import java.net.Socket;
+
+import java.nio.charset.StandardCharsets;
+
+public class ServerWork extends Thread implements Runnable{
+    private final Socket clientSocket;
+
+    public ServerWork(Socket clientSocket) {
+        this.clientSocket = clientSocket;
+    }
+
+    @Override
+    public void run() {
+        try {
+            handleClientSockets();
+        }catch (IOException e){
+            e.printStackTrace();
+        }catch (InterruptedException e){
+            e.printStackTrace();
+        }
+    }
+    private void handleClientSockets() throws IOException, InterruptedException{
+        InputStream inputStream = clientSocket.getInputStream();
+        OutputStream outputStream = clientSocket.getOutputStream();
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+        String textLine;
+        while((textLine = reader.readLine()) != null){
+            if ("exit".equalsIgnoreCase(textLine)){
+               break;
+           }
+           String message = "You said: " + textLine + "\r\n";
+           outputStream.write(message.getBytes());
+        }
+
+        clientSocket.close();
+
+    }
+}
